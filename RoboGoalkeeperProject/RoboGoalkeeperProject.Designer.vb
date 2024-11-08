0997a53c8aeafaba7f7d636246c64beb8c4d00e9
Rahiel Rodriguez <rahielrr@gmail.com>
2024-11-08 13:05:03 -0700

    Soccer Ball tracking and displaying updated. Now tracks showing a soccer ball.

--- RoboGoalkeeperProject/RoboGoalkeeperProject.Designer.vb
+++ RoboGoalkeeperProject/RoboGoalkeeperProject.Designer.vb
@@ -28,6 +28,8 @@ Partial Class RoboGoalkeeperProject
         Me.ComButton = New System.Windows.Forms.Button()
         Me.PositionPictureBox = New System.Windows.Forms.PictureBox()
         Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ShowImageButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
         CType(Me.PositionPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -70,11 +72,31 @@ Partial Class RoboGoalkeeperProject
         Me.ClearButton.Text = "Clear"
         Me.ClearButton.UseVisualStyleBackColor = True
         '
+        'ShowImageButton
+        '
+        Me.ShowImageButton.Location = New System.Drawing.Point(682, 177)
+        Me.ShowImageButton.Name = "ShowImageButton"
+        Me.ShowImageButton.Size = New System.Drawing.Size(143, 83)
+        Me.ShowImageButton.TabIndex = 5
+        Me.ShowImageButton.Text = "Display"
+        Me.ShowImageButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(682, 88)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(143, 83)
+        Me.ExitButton.TabIndex = 6
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
         'RoboGoalkeeperProject
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(837, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ShowImageButton)
         Me.Controls.Add(Me.ClearButton)
         Me.Controls.Add(Me.PositionPictureBox)
         Me.Controls.Add(Me.ComButton)
@@ -91,4 +113,6 @@ Partial Class RoboGoalkeeperProject
     Friend WithEvents ComButton As Button
     Friend WithEvents PositionPictureBox As PictureBox
     Friend WithEvents ClearButton As Button
+    Friend WithEvents ShowImageButton As Button
+    Friend WithEvents ExitButton As Button
 End Class
--- RoboGoalkeeperProject/RoboGoalkeeperProject.vb
+++ RoboGoalkeeperProject/RoboGoalkeeperProject.vb
@@ -4,32 +4,13 @@ Imports System.Windows.Forms.ComponentModel.Com2Interop
 Imports System.Runtime.InteropServices
 
 Public Class RoboGoalkeeperProject
-    Sub WritePixyData()
-        Dim Data(3) As Byte
-        Data(0) = &HC1
-        Data(1) = &HAE
-        Data(2) = &HE
-        Data(3) = &H0
-        'Data(4) = &HFF
-        'Data(5) = &H0
-        'Data(6) = &H0
-        SerialPort.Write(Data, 0, 4)
-
-        Sleep(50)
-    End Sub
     ''' <summary>
     ''' Read the analog input A1 of the Qy_ board. 
     ''' <br/>
     ''' A01 = 01010001
     ''' </summary>
     ''' <returns>Byte Array</returns>'
-    Function Qy_ReadAnalogInPutA1() As Byte()
-        Dim data(0) As Byte
-        data(0) = &B1010001
-        SerialPort.Write(data, 0, 1)
-        Return data
-    End Function
-
+    Dim image As New Bitmap("C:\Users\Rahi\OneDrive\Desktop\ISU\Robotics\5th Semester\Final Project\RoboGoalkeeperProject\RoboGoalkeeperProject\Resources\Soccer Ball.png")
     Sub SerialConnect(portName As String)
         SerialPort.Close()
         SerialPort.PortName = portName
@@ -45,19 +26,6 @@ Public Class RoboGoalkeeperProject
         Next
 
         PortComboBox.SelectedIndex = 0
-    End Sub
-
-    Private Sub ComButton_Click(sender As Object, e As EventArgs) Handles ComButton.Click
-        GetPorts()
-    End Sub
-    Private Sub PortComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles PortComboBox.SelectedIndexChanged
-        SerialConnect(PortComboBox.SelectedItem)
-    End Sub
-
-    Private Sub SerialComExample_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        GetPorts()
-        Dim data(SerialPort.BytesToRead) As Byte
-        SerialPort.Read(Data, 0, SerialPort.BytesToRead)
     End Sub
 
     Private Sub SerialPort_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort.DataReceived
@@ -85,20 +53,9 @@ Public Class RoboGoalkeeperProject
         Dim pen As New Pen(Color.Black)
         Static oldX#, oldY#
         Dim newX#
-        'g.TranslateTransform(0, 255 * -1)
-
         newX = ((highByte * 256) + lowByte)
-
-        g.DrawLine(pen, CInt(oldX), CInt(oldY), CInt(newX), CInt(newY))
-
-        Try
-            If newX > 0 Then
-                Console.WriteLine()
-            End If
-        Catch ex As Exception
-        End Try
-        'Console.Write($"{newX} , {newY}")
-        'store values for start of next line segment
+        g.FillRectangle(Brushes.Green, CInt(oldX), CInt(oldY), 20, 20)
+        g.DrawImage(image, CInt(newX), CInt(newY), 20, 20)
         oldX = newX
         oldY = newY
 
@@ -106,8 +63,32 @@ Public Class RoboGoalkeeperProject
         g.Dispose()
 
     End Sub
+    Public Shared Function ResizeImage(ByVal InputBitmap As Bitmap, width As Integer, height As Integer) As Bitmap
+        Return New Bitmap(InputBitmap, New Size(width, height))
+    End Function
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         PositionPictureBox.Refresh()
+    End Sub
+    Private Sub ComButton_Click(sender As Object, e As EventArgs) Handles ComButton.Click
+        GetPorts()
+    End Sub
+    Private Sub PortComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles PortComboBox.SelectedIndexChanged
+        SerialConnect(PortComboBox.SelectedItem)
+    End Sub
+
+    Private Sub SerialComExample_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GetPorts()
+        Dim data(SerialPort.BytesToRead) As Byte
+        SerialPort.Read(data, 0, SerialPort.BytesToRead)
+        image = ResizeImage(image, 20, 20)
+        PositionPictureBox.BackColor = Color.Green
+    End Sub
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ShowImageButton_Click(sender As Object, e As EventArgs) Handles ShowImageButton.Click
+        PositionPictureBox.Image = image
     End Sub
 End Class
